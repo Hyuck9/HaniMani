696d2de08b149a47c1bdaf3dbfe8ea95780994f9
Hyuck <lhg1304@nexmore.co.kr>
2022-11-08 17:06:59 +0900

    Compose Init

--- app/src/main/java/io/github/hyuck9/hanimani/MainActivity.kt
+++ app/src/main/java/io/github/hyuck9/hanimani/MainActivity.kt
@@ -1,13 +1,43 @@
 package io.github.hyuck9.hanimani
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import dagger.hilt.android.AndroidEntryPoint
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.hyuck9.hanimani.ui.theme.HaniManiTheme
 
-@AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+		setContent {
+			HaniManiTheme {
+				// A surface container using the 'background' color from the theme
+				Surface(
+					modifier = Modifier.fillMaxSize(),
+					color = MaterialTheme.colorScheme.background
+				) {
+					Greeting("Android")
+				}
+			}
+		}
+	}
+}
+
+@Composable
+fun Greeting(name: String) {
+	Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+	HaniManiTheme {
+		Greeting("Android")
 	}
 }
