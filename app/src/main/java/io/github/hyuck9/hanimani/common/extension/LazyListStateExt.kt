3950916b3b0c07df6f284e158f55ed1b8753a0e9
Hyuck9 <lhg1304@nexmore.co.kr>
2022-11-23 13:38:40 +0900

    o FAB (할일 추가 버튼) 추가

---
+++ app/src/main/java/io/github/hyuck9/hanimani/common/extension/LazyListStateExt.kt
@@ -0,0 +1,22 @@
+package io.github.hyuck9.hanimani.common.extension
+
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.runtime.*
+
+@Composable
+fun LazyListState.isScrollingUp(): Boolean {
+	var previousIndex by remember(this) { mutableStateOf(firstVisibleItemIndex) }
+	var previousScrollOffset by remember(this) { mutableStateOf(firstVisibleItemScrollOffset) }
+	return remember(this) {
+		derivedStateOf {
+			if (previousIndex != firstVisibleItemIndex) {
+				previousIndex > firstVisibleItemIndex
+			} else {
+				previousScrollOffset >= firstVisibleItemScrollOffset
+			}.also {
+				previousIndex = firstVisibleItemIndex
+				previousScrollOffset = firstVisibleItemScrollOffset
+			}
+		}
+	}.value
+}
--- app/src/main/java/io/github/hyuck9/hanimani/features/tasks/ui/TasksScreen.kt
+++ app/src/main/java/io/github/hyuck9/hanimani/features/tasks/ui/TasksScreen.kt
@@ -1,11 +1,88 @@
 package io.github.hyuck9.hanimani.features.tasks.ui
 
-import androidx.compose.material3.Text
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
+import io.github.hyuck9.hanimani.R
+import io.github.hyuck9.hanimani.common.extension.isScrollingUp
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TasksScreen(
 	viewModel: TasksViewModel
 ) {
-	Text(text = "Hello HaniMani!!!!")
+
+	val lazyListState = rememberLazyListState()
+	val coroutineScope = rememberCoroutineScope()
+
+	Scaffold(
+		floatingActionButton = {
+			AddTaskButton(
+				extended = lazyListState.isScrollingUp(),
+				onClick = {
+					coroutineScope.launch {
+						// TODO: 할일 추가
+					}
+				}
+			)
+		}
+	) { padding ->
+		Text(
+			text = "Hello HaniMani!!!!",
+			modifier = Modifier.padding(padding)
+		)
+	}
+}
+
+@Composable
+private fun AddTaskButton(
+	extended: Boolean,
+	onClick: () -> Unit
+) {
+	FloatingActionButton(onClick = onClick) {
+		Row(
+			modifier = Modifier.padding(horizontal = 16.dp)
+		) {
+			Icon(
+				imageVector = Icons.Default.Add,
+				contentDescription = stringResource(id = R.string.button_add_task)
+			)
+
+			AnimatedVisibility(visible = extended) {
+				Text(
+					text = stringResource(id = R.string.button_add_task),
+					modifier = Modifier.padding(start = 8.dp, top = 3.dp)
+				)
+			}
+		}
+	}
+}
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun AddTaskButtonPreview(@PreviewParameter(SampleExtendedProvider::class) extended: Boolean) {
+	AddTaskButton(
+		extended = extended,
+		onClick = {}
+	)
+}
+
+private class SampleExtendedProvider : PreviewParameterProvider<Boolean> {
+	override val values: Sequence<Boolean> = sequenceOf(true, false)
 }
